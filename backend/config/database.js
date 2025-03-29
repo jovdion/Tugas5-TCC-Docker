a86479a34a8294530422e52b91ca3758c563a0b2
jovano dion manuel <162147934+jovdion@users.noreply.github.com>
2025-03-29 20:21:27 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('api_catatan', 'root', '',{
-    host: '34.171.244.192',
+const db = new Sequelize('RECOVER_YOUR_DATA', 'root', '',{
+    host: '34.28.222.95',
     dialect: 'mysql'
 });
 
